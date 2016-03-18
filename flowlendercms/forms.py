2d782b5a6a7f67dbd3abe1caadba4ef7d316c225
mcashif <mcashif@hotmail.com>
2016-03-19 01:49:32 +0500

    Fully Dynamic minor-6

--- flowlendercms/forms.py
+++ flowlendercms/forms.py
@@ -4,3 +4,4 @@ from flowlendercms.models import ClientDetail
 class ClientDetailForm(forms.ModelForm):
     class Meta:
         model = ClientDetail # Your User model
+        fields = ('title', 'text',)
